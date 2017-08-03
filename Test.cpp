0baabf05101f64c60da968e7af1be22b229f5a29
andres5892 <andresgomes5892@gmail.com>
2017-08-03 10:52:03 -0500

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <chrono>
+#include <cstdlib>
+#include <cstring>
+#include "MaxSubSeq.h"
+
+std::vector<int> llenarVector(int numPeriodos);
+
+int main( int argc, char* argv[] )
+{
+    int numPeriodos;
+    std::cin>>numPeriodos;
+    
+    
+ 
+std::vector<int> llenarVector(int numPeriodos)
+{
+    int num;
+    for(int i = 1; i <= n; i++)
+    {
+        num = (rand() % 999 + 1);
+    }
+}
