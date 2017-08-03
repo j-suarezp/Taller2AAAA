541453423f0516b5c8f205d45c86ccc8647296b5
j-suarezp <jsuarezp0624@gmail.com>
2017-08-03 10:34:00 -0500

    Update MaxSubSeq.cpp
    Fuerza bruta v1

--- MaxSubSeq.cpp
+++ MaxSubSeq.cpp
@@ -1,1 +1,25 @@
+#include "MaxSubSeq.h"
 
+std::pair<int, int> MaxSubSeq::fuerzaBruta()
+{
+  int inicioMax, finMax, valorMax;
+  valorMax = -2147483647;
+  std::pair<int, int> ret;
+  
+  for(int i = 0; i < this->numPeriodos; i++ )
+  {
+    for(int j = i; j < this->numPeriodos; j++)
+    {
+      if( (this->valores[i] - this->valores[j]) >= valorMax)
+      {
+        inicioMax = i;
+        finMax = j;
+        valorMax = (this->valores[i] - this->valores[j]);
+      }
+    }
+  }
+  
+  ret = std::make_pair(inicioMax, finMax);
+  
+  return( ret );
+}
