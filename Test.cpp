717be39e94db710e729c7306f2fe4999d4272cdf
andres5892 <andresgomes5892@gmail.com>
2017-08-07 17:20:32 -0500

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -10,18 +10,23 @@ int main( int argc, char* argv[] )
 {
     int numPeriodos;
     std::cin>>numPeriodos;
-    llenarVector(numPeriodos);
+    std::vector<int> vect = llenarVector(numPeriodos);
+    
+    for(int i = 0; i < numPeriodos; i++)
+	std::cout<<vect[i]<<" ";
     
     return 0;
 }
-    
+
 std::vector<int> llenarVector(int numPeriodos)
 {
     int num;
     std::vector<int> vect;
-    for(int i = 1; i <= numPeriodos; i++)
+    for(int i = 0; i < numPeriodos; i++)
     {
         num = (rand() % 999 + 1);
         vect.push_back(num);
     }
+    
+    return vect;
 }
