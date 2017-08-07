144c85b0518f70a4c10415fd321e90c1b033afd1
andres5892 <andresgomes5892@gmail.com>
2017-08-07 14:49:22 -0500

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -10,14 +10,18 @@ int main( int argc, char* argv[] )
 {
     int numPeriodos;
     std::cin>>numPeriodos;
+    llenarVector(int numPeriodos);
     
+    return 0;
+}
     
- 
 std::vector<int> llenarVector(int numPeriodos)
 {
     int num;
+    std::vector<int> vect;
     for(int i = 1; i <= n; i++)
     {
         num = (rand() % 999 + 1);
+        vect.push(num);
     }
 }
