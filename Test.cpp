f3ab7629061d0f975c32a7b0c675089e0e24a104
j-suarezp <jsuarezp0624@gmail.com>
2017-08-07 21:15:37 -0500

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -8,32 +8,27 @@
 
 int main( int argc, char* argv[] )
 {
-
+	int num = std::atoi(argv[1]);
 	MaxSubSeq seq;
 
-	seq.inicializar(std::atoi(argv[1]));
+	seq.inicializar(num);
 
-
-
-/*	std::vector<int> v = seq.getValores();
-	std::vector<int> d = seq.getDerivadas();
-
-	for(int i = 0; i < std::atoi(argv[1]) ; i++)
-		std::cout<<v[i]<<" ";
-
-	std::cout<<std::endl<<std::endl;
-
-	for(int i = 0; i < std::atoi(argv[1]) ; i++)
-		std::cout<<d[i]<<" ";
-
-	std::cout<<std::endl<<std::endl<<std::endl;
-*/
+	long start_fb = std::chrono::duration_cast< std::chrono::milliseconds >(
+	std::chrono::system_clock::now( ).time_since_epoch( ) ).count( );
 	std::tuple<int, int, int> fb = seq.fuerzaBruta();
-	std::tuple<int, int, int> dv = seq.findMaxSubArray(0, std::atoi(argv[1])-1);
+	long end_fb = std::chrono::duration_cast< std::chrono::milliseconds >(
+    std::chrono::system_clock::now( ).time_since_epoch( )).count( );
+  	long time_fb = end_fb - start_fb;
+	
+	long start_dv = std::chrono::duration_cast< std::chrono::milliseconds >(
+	std::chrono::system_clock::now( ).time_since_epoch( ) ).count( );
+	std::tuple<int, int, int> dv = seq.findMaxSubArray(0, num-1);
 	std::get<0>(dv)--;
-
-	std::cout<<"Fuerza bruta Inicio: "<<std::get<0>(fb)<<"   Fin: "<<std::get<1>(fb)<<"   Resultado: "<<std::get<2>(fb)<<std::endl;
-	std::cout<<"Div y vencer Inicio: "<<std::get<0>(dv)<<"   Fin: "<<std::get<1>(dv)<<"   Resultado: "<<std::get<2>(dv)<<std::endl;
+	long end_dv = std::chrono::duration_cast< std::chrono::milliseconds >(
+    std::chrono::system_clock::now( ).time_since_epoch( )).count( );
+  	long time_dv = end_dv - start_dv;
+	
+	std::cout<<num<<" "<<std::get<2>(fb)<<" "<<std::get<2>(dv)<<" "<<time_fb<<" "<<time_dv;
 
     return (0);
 }
